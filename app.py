70cc91b141827e67bfb87272e28257e2e1491752
Abdullah <abdullahhamid2395@gmail.com>
2025-02-05 16:22:40 +0500

    created mvp of news summarizer app

---
+++ app.py
@@ -0,0 +1,23 @@
+from textblob import TextBlob
+from newspaper import Article
+import nltk
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route("/",methods=['GET'])
+def search():
+    return render_template("index.html")
+@app.route("/show",methods=["POST"])
+def show():
+    url=request.form['link']
+    article=Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    analysis=TextBlob(article.text)
+    data=[article.title,article.publish_date,article.authors,article.summary,article.keywords,analysis.polarity]
+    return render_template("index.html",data=data)
+
+
+
+
+
